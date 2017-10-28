1c3f147461b769db3bcb4b09ca031eb9d3ec72c9
KyleBanks <kyle@nthgensoftware.com>
2017-10-28 12:47:26 -0400

    Yahoo Finance v8 implementation

--- yf.go
+++ yf.go
@@ -2,94 +2,71 @@ package yf
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"text/template"
-	"time"
 )
 
 const (
-	yahooFinanceApiTemplate = "https://query1.finance.yahoo.com/v7/finance/download/{{.Symbol}}?period1={{.Start}}&period2={{.End}}&interval={{.Interval}}&events={{.Events}}&crumb={{.Crumb}}"
-	defaultInterval         = "1d"
-	defaultEvents           = "history"
+	yahooFinanceAPI = "https://query1.finance.yahoo.com/v8/finance/chart/{{.Symbol}}?range={{.Range}}&includePrePost=false&interval={{.Interval}}&corsDomain=finance.yahoo.com&.tsrc=finance"
 )
 
 var (
+	// DebugLogging enables verbose output from the yf package when set to true.
 	DebugLogging = false
-
-	yahooFinanceApi = template.Must(template.New("YF-API").Parse(yahooFinanceApiTemplate))
 )
 
-type params struct {
-	Symbol string
-
-	Start    int64
-	End      int64
-	Interval string
-
-	Events string
-
-	Crumb string
-}
-
-func GetStock(symbol string, start, end time.Time) error {
-	req, err := getRequest(symbol, start, end)
+// GetStock returns stock details for a particular symbol from the Yahoo Finance API.
+func GetStock(symbol, dateRange, interval string) (*Stock, error) {
+	resp, err := doRequest(symbol, dateRange, interval)
 	if err != nil {
-		return err
-	}
-
-	var c http.Client
-	resp, err := c.Do(req)
-	if err != nil {
-		return err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
 	if resp.StatusCode != 200 {
-		return fmt.Errorf("Unexpected Response: %d: %v", resp.StatusCode, string(body))
+		return nil, fmt.Errorf("Unexpected StatusCode: %d", resp.StatusCode)
 	}
 
-	return nil
+	var target struct {
+		Chart struct {
+			Result []Stock
+		}
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&target); err != nil {
+		return nil, err
+	}
+
+	if len(target.Chart.Result) != 1 {
+		return nil, fmt.Errorf("Unexpected response, %d results returned, API change must have occurred", len(target.Chart.Result))
+	}
+
+	return &target.Chart.Result[0], nil
 }
 
-func getRequest(symbol string, start, end time.Time) (*http.Request, error) {
-	p := params{
+func doRequest(symbol, dateRange, interval string) (*http.Response, error) {
+	tmpl, err := template.New("YF-API").Parse(yahooFinanceAPI)
+	if err != nil {
+		return nil, err
+	}
+
+	p := struct {
+		Symbol   string
+		Range    string
+		Interval string
+	}{
 		Symbol:   symbol,
-		Start:    start.Unix(),
-		End:      end.Unix(),
-		Interval: defaultInterval,
-		Events:   defaultEvents,
+		Range:    dateRange,
+		Interval: interval,
 	}
-
-	crumb, err := getCrumb(symbol)
-	if err != nil {
-		return nil, err
-	}
-	p.Crumb = crumb
-
 	var url bytes.Buffer
-	if err := yahooFinanceApi.Execute(&url, p); err != nil {
+	if err := tmpl.Execute(&url, p); err != nil {
 		return nil, err
 	}
+
 	debug(url.String())
-
-	req, err := http.NewRequest("GET", url.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := addCookie(symbol, req); err != nil {
-		return nil, err
-	}
-	debug(fmt.Sprintf("Headers: %v", req.Header))
-
-	return req, nil
+	return http.Get(url.String())
 }
 
 func debug(str string) {
--- yf_test.go
+++ yf_test.go
@@ -2,7 +2,6 @@ package yf
 
 import (
 	"testing"
-	"time"
 )
 
 func init() {
@@ -11,11 +10,13 @@ func init() {
 
 func TestGetStock(t *testing.T) {
 	symbol := "GOOG"
-	start := time.Now().AddDate(0, -3, 0)
-	end := time.Now()
 
-	err := GetStock(symbol, start, end)
+	stock, err := GetStock(symbol, RangeOneYear, IntervalOneDay)
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	if stock.Meta.Symbol != symbol {
+		t.Fatalf("Unexpected Symbol: expected=%v, got=%v", symbol, stock.Meta.Symbol)
 	}
 }
